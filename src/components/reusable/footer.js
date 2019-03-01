c48b5ba0a930f169f610f78f31d3d302c4d4bdb1
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-01 10:10:35 -0800

    created footer

---
+++ src/components/reusable/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./footer.css";
+
+export function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer-text">© 2019 Noire Coco. All Rights Reserved</p>
+    </div>
+  );
+}
