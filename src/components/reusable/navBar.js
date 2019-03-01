db37e86441f8104fdac1f50209db08672d847cd8
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-01 08:48:52 -0800

    created navigation bar

---
+++ src/components/reusable/navBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "./navBar.css";
+
+export function NavBar(props) {
+  return (
+    <nav>
+      <img
+        className="logo"
+        src="https://i.ibb.co/VNZVnvr/imageedit-35-5868682626.png"
+        alt="Noire Coco logo"
+      />
+      <ul className="nav-links">
+        <a href="#" className="link" aria-label="Shop">
+          Shop
+        </a>
+        <a href="#" className="link" aria-label="New">
+          New
+        </a>
+        <a href="#" className="link" aria-label="About">
+          About
+        </a>
+      </ul>
+    </nav>
+  );
+}
