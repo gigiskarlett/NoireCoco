722d5f4e908af57e9982e567ba2a2f83f28290c1
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-01 09:01:08 -0800

    created page + added imports+ created about us section

---
+++ src/components/pages/about-us/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import "./about-us.css";
+import { NavBar } from "../../reusable/navBar";
+import { ReusableBanner } from "../../reusable/reusable-banner";
+import { Button } from "../../reusable/reusable-button";
+import { Footer } from "../../reusable/footer";
+
+export function AboutUs(props) {
+  return (
+    <React.Fragment>
+      <NavBar />
+      <ReusableBanner />
+      <section className="picture" />
+      <section className="about-us-section">
+        <p className="about-us-paragraph">
+          Noire Coco was created by a beach lover who’s always on the search for
+          swimwear for her next vacation. I’ve always struggled on finding a
+          nice swimwear and it seems that at times I stumbled upon having too
+          many or too little options for pieces that I didn’t really like. I
+          wanted to create a page where finding a beautiful swimsuit for a
+          reasonable price wouldn’t be a search down hundreds of pages. I wanted
+          to make beautiful swimwear with reasonable prices accessible to every
+          women who finds herself always on a quest when trying to find
+          swimwear.
+        </p>
+        <Button/>
+        <div className="empty-space"></div>
+        <Footer/>
+      </section>
+    </React.Fragment>
+  );
+}
