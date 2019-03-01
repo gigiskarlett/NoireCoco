bdb50632afa3e53d6f3cc1f40686b0d7387d58a5
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-01 13:58:36 -0800

    removed dummy data + imported inventory item with data

--- src/components/pages/inventory/index.js
+++ src/components/pages/inventory/index.js
@@ -4,80 +4,26 @@ import "./index.css";
 import { NavBar } from "../../reusable/navBar";
 import { ReusableBanner } from "../../reusable/reusable-banner";
 import { Footer } from "../../reusable/footer";
+import { InventoryItem } from "../../reusable/inventory-item";
 
-export class Inventory extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <NavBar />
-        <ReusableBanner />
-        <div className="add-button-container">
-          <button className="add-button">
-            <img
-              className="plus-icon"
-              src="https://i.ibb.co/wYrwDgM/icons8-plus-math-60.png"
-              alt="plus-icon"
-            />
-            ADD NEW
-          </button>
-        </div>
-        <section className="list-of-products">
-          <section className="product">
-            <section className="name">
-              <h4>NAME</h4>
-              <p className="inventory-list"> BLABLABLA BIKINI</p>
-            </section>
-            <section className="image-url">
-              <h4>IMAGE URL</h4>
-              <p className="inventory-list"> SOME URLS</p>
-            </section>
-            <section className="description">
-              <h4>DESCRIPTION</h4>
-              <p className="inventory-list">
-                High Rise Thong Bikini Bottoms...
-              </p>
-            </section>
-            <section className="price">
-              <h4>PRICE</h4>
-              <p className="inventory-list">$30.00</p>
-            </section>
-            <input
-              type="image"
-              src="https://i.ibb.co/jRXb1ng/icons8-delete-60-1.png"
-              className="delete-icon"
-              alt="delete icon"
-            />
-          </section>
-          <section className="product">
-            <section className="name">
-              <h4>NAME</h4>
-              <p className="inventory-list"> BLABLABLA BIKINI</p>
-            </section>
-            <section className="image-url">
-              <h4>IMAGE URL</h4>
-              <p className="inventory-list"> SOME URLS</p>
-            </section>
-            <section className="description">
-              <h4>DESCRIPTION</h4>
-              <p className="inventory-list">
-                High Rise Thong Bikini Bottoms...
-              </p>
-            </section>
-            <section className="price">
-              <h4>PRICE</h4>
-              <p className="inventory-list">$30.00</p>
-            </section>
-            <input
-              type="image"
-              src="https://i.ibb.co/jRXb1ng/icons8-delete-60-1.png"
-              className="delete-icon"
-              alt="delete icon"
-            />
-          </section>
-        </section>
-        <div className="empty-space" />
-        <Footer />
-      </React.Fragment>
-    );
-  }
+export function Inventory(props) {
+  return (
+    <React.Fragment>
+      <NavBar />
+      <ReusableBanner />
+      <div className="add-button-container">
+        <button className="add-button">
+          <img
+            className="plus-icon"
+            src="https://i.ibb.co/wYrwDgM/icons8-plus-math-60.png"
+            alt="plus-icon"
+          />
+          ADD NEW
+        </button>
+      </div>
+      <InventoryItem />
+      <div className="empty-space" />
+      <Footer />
+    </React.Fragment>
+  );
 }
