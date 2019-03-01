3e07bf6db8a8f8c954810dec984c30fed9137c77
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-01 08:51:10 -0800

    created landing page

--- src/components/pages/landing/index.js
+++ src/components/pages/landing/index.js
@@ -1,12 +1,32 @@
 import React from "react";
-import { connect } from "react-redux";
-import { Link, Redirect } from "react-router-dom";
 
-export default function LandingPage(props) {
-  return (
-    <div className="home">
-      <h2>Do GOOD</h2>
-      <p>landing here</p>
-    </div>
-  );
+import { NavBar } from "../elements/navBar";
+import { InstagramBanner } from "../elements/instagram-banner";
+import { Footer } from "../elements/footer";
+import "./index.css";
+import { EmailCapture } from "../elements/email-capture";
+
+export default class LandingPage extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <div className="about-us-banner">
+          <p className="catchy-phrase">SWIM</p>
+          <p className="catchy-phrase">
+            There’s nothing better than wearing the perfect swimwear to go to
+            the pool or the beach, that’s why we have selected our favorite
+            selection of swimwear for you
+          </p>
+        </div>
+        <div className="landing-banner">
+          <h2>shop new styles</h2>
+        </div>
+        <section className="shop-by-style" />
+        <InstagramBanner />
+        <EmailCapture />
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
