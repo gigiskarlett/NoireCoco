3fbb8bf864b2b754ef4bc668eda726aea19c26ca
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-01 13:55:44 -0800

    created product styling for the inventory + added dummy data

--- src/components/reusable/inventory-item.js
+++ src/components/reusable/inventory-item.js
@@ -1,3 +1,148 @@
 import React from "react";
 
 import "./inventory-item.css";
+
+export class InventoryItem extends React.Component {
+  render() {
+    return (
+      <section className="list-of-products">
+      <section className="product">
+      <section className="name">
+        <h4>NAME</h4>
+        <p className="inventory-list"> BLABLABLA BIKINI</p>
+      </section>
+      <section className="image-url">
+        <h4>IMAGE URL</h4>
+        <p className="inventory-list"> SOME URLS</p>
+      </section>
+      <section className="description">
+        <h4>DESCRIPTION</h4>
+        <p className="inventory-list">
+          High Rise Thong Bikini Bottoms with styleHigh Rise Thong Bikini
+          Bottoms with styleHigh Rise Thong Bikini Bottoms with styleHigh
+          Rise Thong Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms
+          with styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
+          Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms with
+          styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
+          Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms with
+          styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
+          Bikini Bottoms with style{" "}
+        </p>
+      </section>
+      <section className="price">
+        <h4>PRICE</h4>
+        <p className="inventory-list">$30.00</p>
+      </section>
+      <input
+        type="image"
+        src="https://i.ibb.co/jRXb1ng/icons8-delete-60-1.png"
+        className="delete-icon"
+        alt="delete icon"
+      />
+    </section>
+     <section className="product">
+      <section className="name">
+        <h4>NAME</h4>
+        <p className="inventory-list"> BLABLABLA BIKINI</p>
+      </section>
+      <section className="image-url">
+        <h4>IMAGE URL</h4>
+        <p className="inventory-list"> SOME URLS</p>
+      </section>
+      <section className="description">
+        <h4>DESCRIPTION</h4>
+        <p className="inventory-list">
+          High Rise Thong Bikini Bottoms with styleHigh Rise Thong Bikini
+          Bottoms with styleHigh Rise Thong Bikini Bottoms with styleHigh
+          Rise Thong Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms
+          with styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
+          Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms with
+          styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
+          Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms with
+          styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
+          Bikini Bottoms with style{" "}
+        </p>
+      </section>
+      <section className="price">
+        <h4>PRICE</h4>
+        <p className="inventory-list">$30.00</p>
+      </section>
+      <input
+        type="image"
+        src="https://i.ibb.co/jRXb1ng/icons8-delete-60-1.png"
+        className="delete-icon"
+        alt="delete icon"
+      />
+    </section>
+        <section className="product">
+          <section className="name">
+            <h4>NAME</h4>
+            <p className="inventory-list"> BLABLABLA BIKINI</p>
+          </section>
+          <section className="image-url">
+            <h4>IMAGE URL</h4>
+            <p className="inventory-list"> SOME URLS</p>
+          </section>
+          <section className="description">
+            <h4>DESCRIPTION</h4>
+            <p className="inventory-list">
+              High Rise Thong Bikini Bottoms with styleHigh Rise Thong Bikini
+              Bottoms with styleHigh Rise Thong Bikini Bottoms with styleHigh
+              Rise Thong Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms
+              with styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
+              Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms with
+              styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
+              Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms with
+              styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
+              Bikini Bottoms with style{" "}
+            </p>
+          </section>
+          <section className="price">
+            <h4>PRICE</h4>
+            <p className="inventory-list">$30.00</p>
+          </section>
+          <input
+            type="image"
+            src="https://i.ibb.co/jRXb1ng/icons8-delete-60-1.png"
+            className="delete-icon"
+            alt="delete icon"
+          />
+        </section>
+         <section className="product">
+          <section className="name">
+            <h4>NAME</h4>
+            <p className="inventory-list"> BLABLABLA BIKINI</p>
+          </section>
+          <section className="image-url">
+            <h4>IMAGE URL</h4>
+            <p className="inventory-list"> SOME URLS</p>
+          </section>
+          <section className="description">
+            <h4>DESCRIPTION</h4>
+            <p className="inventory-list">
+              High Rise Thong Bikini Bottoms with styleHigh Rise Thong Bikini
+              Bottoms with styleHigh Rise Thong Bikini Bottoms with styleHigh
+              Rise Thong Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms
+              with styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
+              Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms with
+              styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
+              Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms with
+              styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
+              Bikini Bottoms with style{" "}
+            </p>
+          </section>
+          <section className="price">
+            <h4>PRICE</h4>
+            <p className="inventory-list">$30.00</p>
+          </section>
+          <input
+            type="image"
+            src="https://i.ibb.co/jRXb1ng/icons8-delete-60-1.png"
+            className="delete-icon"
+            alt="delete icon"
+          />
+        </section>
+      </section>
+    );
+  }
+}
