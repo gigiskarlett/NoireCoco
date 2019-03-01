f94ed1b6401a7fdc7e6effc42b7fad7e9560b1e6
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-01 09:54:36 -0800

    created email capture

---
+++ src/components/reusable/email-capture.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import "./email-capture.css";
+
+export function EmailCapture() {
+  return (
+    <div className="container">
+      <div className="email-box-container">
+        <p className="call-to-action">Lets keep in touch</p>
+        <section>
+          <form className="email-capture" action="#">
+            <legend align="center" hidden>
+              Lets keep in touch? Subscribe
+            </legend>
+            <input
+              type="text"
+              placeholder="Enter your email address"
+              name="email-address-input"
+              className="email-address-input"
+              required
+            />
+            <input type="submit" className="join-button" value="Join" />
+          </form>
+        </section>
+      </div>
+    </div>
+  );
+}
