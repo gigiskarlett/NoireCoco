d4316a5eb85b648cc9c68e35b7b969c1b2f05f54
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-04 10:14:15 -0800

    layout for mobile view of product

--- src/components/reusable/product-images-mobile-view.js
+++ src/components/reusable/product-images-mobile-view.js
@@ -1,4 +1,32 @@
 import React from "react";
 import "./product-images-mobile-view.css";
 
-export function ProductImagesMobileView(props) {}
+//Renders mobile viewport layout for product images
+export function ProductImagesMobileView(props) {
+    return (
+        <React.Fragment>
+          <div className="images-container">
+            <div className="hero-image">
+              <img 
+                src="https://i.ibb.co/vPVX4tQ/2019-sexy-black-yellow-high-waist-cut-out-trikini-ribbed-bathing-suit-monokini-hollow-out-shoulder-swimwear-one-piece-swimsuit-1.jpg "
+                alt="igh-waist-cut-out-trikini-ribbed-bathing-suit."
+              />
+            </div>
+            <div className="bottom-thumbnails-container">
+              <a className="thumbnails" href="#">
+                <img 
+                  src="https://i.ibb.co/ft853Pz/2019-sexy-black-yellow-high-waist-cut-out-trikini-ribbed-bathing-suit-monokini-hollow-out-shoulder-swimwear-one-piece-swimsuit-2.jpg"
+                  alt="trikini-ribbed-bathing-suit-monokini-hollow-out-shoulder-swimwear-one-piece-swimsuit."
+                />
+              </a>
+              <a className="thumbnails" href="#">
+                <img 
+                  src="https://i.ibb.co/9qZdCqQ/2019-sexy-black-yellow-high-waist-cut-out-trikini-ribbed-bathing-suit-monokini-hollow-out-shoulder-swimwear-one-piece-swimsuit-3.jpg"
+                  alt="trikini-ribbed-bathing-suit-monokini-hollow-out-shoulder-swimwear-one-piece-swimsuit"
+                />
+              </a>
+            </div>
+          </div>
+        </React.Fragment>
+      );
+}
