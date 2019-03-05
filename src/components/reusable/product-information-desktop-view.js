00b1cc7d94998363b45fffad7f00eddd476ee588
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-04 20:38:24 -0800

    added decorative line for details

--- src/components/reusable/product-information-desktop-view.js
+++ src/components/reusable/product-information-desktop-view.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {Button} from './reusable-button'
+import { Button } from "./reusable-button";
 import "./product-information-desktop-view.css";
 
 //Renders information about product
@@ -17,18 +17,21 @@ export class ProductInformationDesktopView extends React.Component {
           STRAPPY HIGH RISE THONG CUT BIKINI BOTTOMS IN WHITE
         </h2>
         <h4 className="price">$40.00</h4>
-          <h3 className="colour-heading">COLOUR</h3>
-          <p className="colour">WHITE</p>
+        <h3 className="colour-heading">COLOUR</h3>
+        <p className="colour">WHITE</p>
         <div className="product-details">
-            <h3 className="details-heading">DETAILS</h3>
-            <p className="details">Fabric: 80% chinlon,20% spandex
-            Design: Falbala design Tie at back
-            About Cup Style: Padding bra
-            Garment Care: Hand Wash and Hang Dry. Recommend with Cold Water. Do not Use Bleach.
-            </p>
+          <h3 className="details-heading">DETAILS</h3>
+          <hr color="#FF70BD" />
+          <p className="details">
+            Fabric: 80% chinlon,20% spandex Design: Falbala design Tie at back
+            About Cup Style: Padding bra Garment Care: Hand Wash and Hang Dry.
+            Recommend with Cold Water. Do not Use Bleach.
+          </p>
         </div>
         <Button />
       </div>
     );
   }
 }
+
+//pass state+props from db item
