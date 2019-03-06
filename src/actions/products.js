1ef0782e5cb4a26d1964addc827410b1a3850507
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-06 14:30:45 -0800

    get route

---
+++ src/actions/products.js
@@ -0,0 +1,18 @@
+import { API_BASE_URL } from "../config";
+
+export const getAllProducts = products => dispatch => {
+  return fetch(`${API_BASE_URL}/api/products/`, {
+    method: "GET",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify(products)
+  })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: "something went terribly wrong" });
+    });
+};
+
