5a4a822191b3edc9f058960b1edbdb34af17f53d
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-07 10:54:12 -0800

    fixed fetch call + create action for success + created action for error

--- src/actions/products.js
+++ src/actions/products.js
@@ -1,50 +1,97 @@
 import { API_BASE_URL } from "../config";
+import { normalizeResponseErrors } from "./utils";
+
+export const FETCH_PRODUCTS_SUCCESS = "FETCH_PRODUCTS_SUCCESS";
+export const fetchProductsSuccess = data => ({
+  type: FETCH_PRODUCTS_SUCCESS,
+  data
+});
+
+export const FETCH_PRODUCTS_ERROR = "FETCH_PRODUCTS_ERROR";
+export const fetchProductsError = error => ({
+  type: FETCH_PRODUCTS_ERROR,
+  error
+});
 
 export const getAllProducts = products => dispatch => {
-  return fetch(`${API_BASE_URL}/api/products/`, {
+  return fetch(`${API_BASE_URL}/products/`, {
     method: "GET",
     headers: {
       "content-type": "application/json"
-    },
-    body: JSON.stringify(products)
+    }
   })
     .then(res => normalizeResponseErrors(res))
     .then(res => res.json())
+    .then(data => dispatch(fetchProductsSuccess(data)))
     .catch(err => {
-      console.error(err);
-      res.status(500).json({ error: "something went terribly wrong" });
+      console.log("Whoops! Try this again.");
+      dispatch(fetchProductsError(err));
     });
 };
 
-export const postProduct = product => dispatch => {
-    return fetch(`${API_BASE_URL}/api/products/`, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json"
-      },
-      body: JSON.stringify(product)
-    })
-      .then(res => normalizeResponseErrors(res))
-      .then(res => res.json())
-      .catch(err => {
-        console.error(err);
-        res.status(500).json({ error: "something went terribly wrong" });
-      });
-  };
+// // export const getOneProduct = product => dispatch => {
+// //     return fetch(`${API_BASE_URL}/api/products/`, {
+// //       method: "GET",
+// //       headers: {
+// // 'Accept': 'application/json',
+// //         "content-type": "application/json"
+// //       },
+// //       body: JSON.stringify(product)
+// //     })
+// //       .then(res => normalizeResponseErrors(res))
+// //       .then(res => res.json())
+// //       .catch(err => {
+// //         console.error(err);
+// //         res.status(500).json({ error: "something went terribly wrong" });
+// //       });
+// //   };
 
-  
-  export const deleteProduct = product => dispatch => {
-      return fetch(`${API_BASE_URL}/api/products/`, {
-        method: "DELETE",
-        headers: {
-          "content-type": "application/json"
-        },
-        body: JSON.stringify(product)
-      })
-        .then(res => normalizeResponseErrors(res))
-        .then(res => res.json())
-        .catch(err => {
-          console.error(err);
-          res.status(500).json({ error: "something went terribly wrong" });
-        });
-    };
+// export const postProduct = product => dispatch => {
+//   return fetch(`${API_BASE_URL}/api/products/`, {
+//     method: "POST",
+//     headers: {
+//       "content-type": "application/json"
+//     },
+//     body: JSON.stringify(product)
+//   })
+//     .then(res => normalizeResponseErrors(res))
+//     .then(res => res.json())
+//     .catch(err => {
+//       console.error(err);
+//     });
+// };
+
+// //success and error action
+// //get all
+// //store
+// //
+
+// // export const updateProduct = product => dispatch => {
+// //   return fetch(`${API_BASE_URL}/api/products/`, {
+// //     method: "PUT",
+// //     headers: {
+// //       "content-type": "application/json"
+// //     },
+// //     body: JSON.stringify(product)
+// //   })
+// //     .then(res => normalizeResponseErrors(res))
+// //     .then(res => res.json())
+// //     .catch(err => {
+// //       console.error(err);
+// //     });
+// // };
+
+// export const deleteProduct = product => dispatch => {
+//   return fetch(`${API_BASE_URL}/api/products/`, {
+//     method: "DELETE",
+//     headers: {
+//       "content-type": "application/json"
+//     },
+//     body: JSON.stringify(product)
+//   })
+//     .then(res => normalizeResponseErrors(res))
+//     .then(res => res.json())
+//     .catch(err => {
+//       console.error(err);
+//     });
+// };
