4c23ec13f3467219fbc7d5dd8b180bc2458e41c4
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-07 15:27:22 -0800

    connected to store + added state to props

--- src/components/reusable/product.js
+++ src/components/reusable/product.js
@@ -1,32 +1,44 @@
-import React from 'react';
+import React from "react";
 
-import './product.css';
+import { connect } from "react-redux";
+import "./product.css";
 
 //Renders Product with product images and information
 export function Product(props) {
+  //connect to store
+  //map state to props
+  //use this.props.products .map show components
   return (
     <React.Fragment>
-      <div className="products">
+      <div className="products" id={props.products.id}>
         <div className="product-container">
           <img
             className="product-image"
-            src="https://i.ibb.co/w7pHj46/Screen-Shot-2019-02-11-at-8-25-02-AM.png"
+            src={props.products.imageUrl}
             alt="bikini"
           />
-          <h5 className="product-name">
+          <h6 className="product-name">
             <img
               className="heart"
               src="https://i.ibb.co/CbBVXLq/imageedit-16-2644387967.png"
               alt="heart"
             />
-            BLABLABLA BIKINI
-          </h5>
+            {props.products.name}
+          </h6>
           <p className="short-description">
-            High Rise Thong Bikini Bottoms in White
+            {props.products.shortDescription}
           </p>
-          <button className="button">BUY NOW</button>
+          <p className="product-price">{props.products.price}</p>
+          <button className="product-button">BUY NOW</button>
         </div>
       </div>
     </React.Fragment>
   );
 }
+const mapStateToProps = state => ({
+  products: state.products
+});
+
+export default connect(mapStateToProps)(Product);
+
+//pass state+props from db item
