704293caef37eb6469ee2ab5d27b5ae9adce2612
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-11 13:16:37 -0700

    boiler plate for ig banner

---
+++ src/components/reusable/instagram-banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './instagram-banner.css';
+
+//Renders instagram banner with images from websites instagram
+export function InstagramBanner() {
+    return (
+        <div className="instagram-banner"></div>
+    )
+}
+
+//get request to ig api
