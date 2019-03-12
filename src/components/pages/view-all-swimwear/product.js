d9a32e5cd2ee884e2952251ba0c29e554371280d
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-11 21:20:35 -0700

    rmvd commented out code

--- src/components/pages/view-all-swimwear/product.js
+++ src/components/pages/view-all-swimwear/product.js
@@ -5,36 +5,6 @@ import "./product.css";
 
 //Renders Product with product images and short information
 export class Product extends React.Component {
-  // createProduct() {
-  //   return this.props.products.map(product => {
-  //     return (
-  //       <ul>
-  //         <div className="products" id={this.props.id}>
-  //           <div className="product-container">
-  //             <img
-  //               className="product-image"
-  //               src={this.props.imageUrl}
-  //               alt="bikini"
-  //             />
-  //             <h6 className="product-name">
-  //               <img
-  //                 className="heart"
-  //                 src="https://i.ibb.co/CbBVXLq/imageedit-16-2644387967.png"
-  //                 alt="heart"
-  //               />
-  //               {this.props.name}
-  //             </h6>
-  //             <p className="short-description">
-  //               {this.props.shortDescription}
-  //             </p>
-  //             <p className="product-price">{this.props.price}</p>
-  //             <button className="product-button">BUY NOW</button>
-  //           </div>
-  //         </div>
-  //       </ul>
-  //     );
-  //   });
-  // }
   render() {
     const products = this.props.products.map((product, i) => {
       return (
