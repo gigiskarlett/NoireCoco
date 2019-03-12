5c1b2e5d270b7685d1583827c17cca2961d29bee
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-11 21:11:00 -0700

    rmvd duplicate html

--- src/components/pages/view-one-product/index.js
+++ src/components/pages/view-one-product/index.js
@@ -5,34 +5,28 @@ import { ReusableBanner } from "../../reusable/banner";
 import { ProductInformationDesktopView } from "./product-information-desktop-view";
 import { ProductInformationMobileView } from "./product-information-mobile-view";
 import { ProductImagesDesktopView } from "./product-images-desktop-view";
-import {Footer} from "../../reusable/footer"
+import { Footer } from "../../reusable/footer";
 import "./index.css";
 
 //Renders page with product selected
 export function ShowOneProduct(props) {
   return (
     <React.Fragment>
+      <NavBar />
+      <ReusableBanner />
       <div className="desktop-view">
-        <NavBar />
-        <ReusableBanner />
         <div className="product-container-desktop-view">
           <ProductImagesDesktopView />
           <ProductInformationDesktopView />
         </div>
-        <Footer />
       </div>
-//dispatch to get one product
-//import 
-
       <div className="mobile-view">
-        <NavBar />
-        <ReusableBanner />
         <div className="product-container-mobile-view">
           <ProductInformationMobileView />
         </div>
-        <Footer />
       </div>
+      <Footer />
+      //dispatch to get one product //import
     </React.Fragment>
   );
 }
-
