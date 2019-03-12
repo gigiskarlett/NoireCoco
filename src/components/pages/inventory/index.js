36cd1486b538966a646991e77fe2368824f12e01
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-12 09:16:12 -0700

    changed to class+ added dispatch on life cycle event

--- src/components/pages/inventory/index.js
+++ src/components/pages/inventory/index.js
@@ -1,29 +1,39 @@
 import React from "react";
 
-import "./index.css";
+import { connect } from "react-redux";
 import { NavBar } from "../../reusable/navBar";
 import { ReusableBanner } from "../../reusable/banner";
 import { Footer } from "../../reusable/footer";
 import { InventoryItem } from "./item";
+import { getAllProducts } from "../../../actions/products";
 
-export function Inventory(props) {
-  return (
-    <React.Fragment>
-      <NavBar />
-      <ReusableBanner />
-      <div className="add-button-container">
-        <button className="add-button">
-          <img
-            className="plus-icon"
-            src="https://i.ibb.co/wYrwDgM/icons8-plus-math-60.png"
-            alt="plus-icon"
-          />
-          ADD NEW
-        </button>
-      </div>
-      <InventoryItem />
-      <div className="empty-space" />
-      <Footer />
-    </React.Fragment>
-  );
+import "./index.css";
+
+export class Inventory extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(getAllProducts());
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <ReusableBanner />
+        <div className="add-button-container">
+          <button className="add-button">
+            <img
+              className="plus-icon"
+              src="https://i.ibb.co/wYrwDgM/icons8-plus-math-60.png"
+              alt="plus-icon"
+            />
+            ADD NEW
+          </button>
+        </div>
+        <InventoryItem />
+        <div className="empty-space" />
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
+
+export default connect()(Inventory);
