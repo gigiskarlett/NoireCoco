966494a56fb70c1967dfd4c3683ca6ea746510a1
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-12 09:06:56 -0700

    connected to store + added props

--- src/components/pages/inventory/item.js
+++ src/components/pages/inventory/item.js
@@ -1,151 +1,70 @@
 import React from "react";
 
+import { connect } from "react-redux";
 import "./inventory-item.css";
 
 //Renders items in the database
 export class InventoryItem extends React.Component {
   render() {
-    return (
-      <section className="list-of-products">
-      <section className="product">
-      <section className="name">
-        <h4 className="field-header">NAME</h4>
-        <p className="inventory-list"> BLABLABLA BIKINI</p>
-      </section>
-      <section className="image-url">
-        <h4 className="field-header">IMAGE URL</h4>
-        <p className="inventory-list"> SOME URLS</p>
-      </section>
-      <section className="description">
-        <h4 className="field-header">DESCRIPTION</h4>
-        <p className="inventory-list">
-          High Rise Thong Bikini Bottoms with styleHigh Rise Thong Bikini
-          Bottoms with styleHigh Rise Thong Bikini Bottoms with styleHigh
-          Rise Thong Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms
-          with styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
-          Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms with
-          styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
-          Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms with
-          styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
-          Bikini Bottoms with style{" "}
-        </p>
-      </section>
-      <section className="price">
-        <h4 className="field-header">PRICE</h4>
-        <p className="inventory-list">$30.00</p>
-      </section>
-      <input
-        type="image"
-        src="https://i.ibb.co/jRXb1ng/icons8-delete-60-1.png"
-        className="delete-icon"
-        alt="delete icon"
-      />
-    </section>
-     <section className="product">
-      <section className="name">
-        <h4 className="field-header">NAME</h4>
-        <p className="inventory-list"> BLABLABLA BIKINI</p>
-      </section>
-      <section className="image-url">
-        <h4 className="field-header">IMAGE URL</h4>
-        <p className="inventory-list"> SOME URLS</p>
-      </section>
-      <section className="description">
-        <h4 className="field-header">DESCRIPTION</h4>
-        <p className="inventory-list">
-          High Rise Thong Bikini Bottoms with styleHigh Rise Thong Bikini
-          Bottoms with styleHigh Rise Thong Bikini Bottoms with styleHigh
-          Rise Thong Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms
-          with styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
-          Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms with
-          styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
-          Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms with
-          styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
-          Bikini Bottoms with style{" "}
-        </p>
-      </section>
-      <section className="price">
-        <h4 className="field-header">PRICE</h4>
-        <p className="inventory-list">$30.00</p>
-      </section>
-      <input
-        type="image"
-        src="https://i.ibb.co/jRXb1ng/icons8-delete-60-1.png"
-        className="delete-icon"
-        alt="delete icon"
-      />
-    </section>
-        <section className="product">
-          <section className="name">
-            <h4 className="field-header">NAME</h4>
-            <p className="inventory-list"> BLABLABLA BIKINI</p>
+    const products = this.props.products.map((product, i) => {
+      return (
+        <section className="list-of-products">
+          <section className="product" key={i}>
+            <section className="name">
+              <h4 className="field-header">NAME</h4>
+              <p className="inventory-list">{product.name}</p>
+            </section>
+            <section className="url">
+            <h4 className="field-header">URL</h4>
+            <p className="inventory-list">{product.url}</p>
           </section>
-          <section className="image-url">
-            <h4 className="field-header">IMAGE URL</h4>
-            <p className="inventory-list"> SOME URLS</p>
+            <section className="image-url">
+              <h4 className="field-header">IMAGE URL</h4>
+              <p className="inventory-list">{product.imageUrl}</p>
+            </section>
+            <section className="second-image-url">
+            <h4 className="field-header">SECOND IMAGE URL</h4>
+            <p className="inventory-list">{product.secondImageUrl}</p>
           </section>
-          <section className="description">
-            <h4 className="field-header">DESCRIPTION</h4>
-            <p className="inventory-list">
-              High Rise Thong Bikini Bottoms with styleHigh Rise Thong Bikini
-              Bottoms with styleHigh Rise Thong Bikini Bottoms with styleHigh
-              Rise Thong Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms
-              with styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
-              Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms with
-              styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
-              Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms with
-              styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
-              Bikini Bottoms with style{" "}
-            </p>
-          </section>
-          <section className="price">
-            <h4 className="field-header">PRICE</h4>
-            <p className="inventory-list">$30.00</p>
-          </section>
-          <input
-            type="image"
-            src="https://i.ibb.co/jRXb1ng/icons8-delete-60-1.png"
-            className="delete-icon"
-            alt="delete icon"
-          />
+          <section className="third-image-url">
+          <h4 className="field-header">THIRD IMAGE URL</h4>
+          <p className="inventory-list">{product.thirdImageUrl}</p>
         </section>
-         <section className="product">
-          <section className="name">
-            <h4 className="field-header">NAME</h4>
-            <p className="inventory-list"> BLABLABLA BIKINI</p>
+            <section className="item-short-description">
+            <h4 className="field-header">SHORT DESCRIPTION</h4>
+            <p className="inventory-list">{product.shortDescription}</p>
           </section>
-          <section className="image-url">
-            <h4 className="field-header">IMAGE URL</h4>
-            <p className="inventory-list"> SOME URLS</p>
+            <section className="details">
+              <h4 className="field-header">DETAILS</h4>
+              <p className="inventory-list">
+                {product.details}
+              </p>
+            </section>
+            <section className="price">
+              <h4 className="field-header">PRICE</h4>
+              <p className="inventory-list">{product.price}</p>
+            </section>
+            <section className="style">
+            <h4 className="field-header">STYLE</h4>
+            <p className="inventory-list">{product.style}</p>
           </section>
-          <section className="description">
-            <h4 className="field-header">DESCRIPTION</h4>
-            <p className="inventory-list">
-              High Rise Thong Bikini Bottoms with styleHigh Rise Thong Bikini
-              Bottoms with styleHigh Rise Thong Bikini Bottoms with styleHigh
-              Rise Thong Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms
-              with styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
-              Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms with
-              styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
-              Bikini Bottoms with styleHigh Rise Thong Bikini Bottoms with
-              styleHigh Rise Thong Bikini Bottoms with styleHigh Rise Thong
-              Bikini Bottoms with style{" "}
-            </p>
+            <input
+              type="image"
+              src="https://i.ibb.co/jRXb1ng/icons8-delete-60-1.png"
+              className="delete-icon"
+              alt="delete icon"
+            />
           </section>
-          <section className="price">
-            <h4 className="field-header">PRICE</h4>
-            <p className="inventory-list">$30.00</p>
-          </section>
-          <input
-            type="image"
-            src="https://i.ibb.co/jRXb1ng/icons8-delete-60-1.png"
-            className="delete-icon"
-            alt="delete icon"
-          />
         </section>
-      </section>
-    );
+      );
+    });
+
+    return <ul>{products}</ul>
   }
 }
 
-//add state & props
+const mapStateToProps = state => ({
+  products: state.products.products
+});
+
+export default connect(mapStateToProps)(InventoryItem);
