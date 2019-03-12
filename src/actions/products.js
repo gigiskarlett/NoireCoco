a6febb82635b9a17ee601460628b0930c33ccae1
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-12 14:16:53 -0700

    added action for error/succes for post + post api call

--- src/actions/products.js
+++ src/actions/products.js
@@ -1,16 +1,17 @@
 import { API_BASE_URL } from "../config";
 import { normalizeResponseErrors } from "./utils";
 
+//Fetch GET api call
 export const FETCH_PRODUCTS_SUCCESS = "FETCH_PRODUCTS_SUCCESS";
 export const fetchProductsSuccess = products => ({
   type: FETCH_PRODUCTS_SUCCESS,
   products
 });
 
-export const FETCH_PRODUCTS_ERROR = "FETCH_PRODUCTS_ERROR";
-export const fetchProductsError = error => ({
-  type: FETCH_PRODUCTS_ERROR,
-  error
+export const POST_PRODUCTS_SUCCESS = "POST_PRODUCTS_SUCCESS";
+export const postProductsSuccess = products => ({
+  type: POST_PRODUCTS_SUCCESS,
+  products
 });
 
 export const getAllProducts = products => dispatch => {
@@ -46,20 +47,52 @@ export const getAllProducts = products => dispatch => {
 // //       });
 // //   };
 
-// export const postProduct = product => dispatch => {
-//   return fetch(`${API_BASE_URL}/api/products/`, {
-//     method: "POST",
-//     headers: {
-//       "content-type": "application/json"
-//     },
-//     body: JSON.stringify(product)
-//   })
-//     .then(res => normalizeResponseErrors(res))
-//     .then(res => res.json())
-//     .catch(err => {
-//       console.error(err);
-//     });
-// };
+//Fetch Post Api call
+
+export const FETCH_PRODUCTS_ERROR = "FETCH_PRODUCTS_ERROR";
+export const fetchProductsError = error => ({
+  type: FETCH_PRODUCTS_ERROR,
+  error
+});
+
+export const POST_PRODUCTS_ERROR = "POST_PRODUCTS_ERROR";
+export const postProductsError = error => ({
+  type: POST,
+  error
+});
+
+export const postProduct = product => dispatch => {
+  return fetch(`${API_BASE_URL}/api/products/`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      name,
+      imageUrl,
+      secondImage,
+      thirdImage,
+      details,
+      shortDescription,
+      price,
+      style,
+      url
+    })
+  })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .catch(err => {
+      let message;
+      if (err.code === 422) {
+        message = err.message;
+        }  else if (err.code === 500) {
+          message = 'Internal server error';
+        } else {
+          message = 'Something went wrong, please try again later';
+        }
+});
+
+
 
 // //success and error action
 // //get all
