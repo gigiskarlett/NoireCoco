278fc63311d9840639ebe6d38387b1fdd688cde1
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-12 14:28:38 -0700

    imported succes and error action + created reducer item success and err

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,6 +1,8 @@
 import {
   FETCH_PRODUCTS_SUCCESS,
-  FETCH_PRODUCTS_ERROR
+  FETCH_PRODUCTS_ERROR,
+  POST_PRODUCTS_SUCCESS,
+  POST_PRODUCTS_ERROR
 } from "../actions/products";
 
 const initialState = {
@@ -15,6 +17,17 @@ export default function reducer(state = initialState, action) {
       error: null
     });
   } else if (action.type === FETCH_PRODUCTS_ERROR) {
+    return Object.assign({}, state, {
+      error: action.error
+    });
+  }
+  else if (action.type === POST_PRODUCTS_SUCCESS) {
+    return Object.assign({}, state, {
+      items: action.items,
+      error: null
+    });
+  }
+  else if (action.type === POST_PRODUCTS_ERROR) {
     return Object.assign({}, state, {
       error: action.error
     });
