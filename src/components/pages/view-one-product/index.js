1e898043e485a3267e129e9387e03cebc5f5fe93
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-11 18:06:56 -0700

    updated import

--- src/components/pages/view-one-product/index.js
+++ src/components/pages/view-one-product/index.js
@@ -1,27 +1,38 @@
 import React from "react";
 
 import { NavBar } from "../../reusable/navBar";
-import { ReusableBanner } from "../../reusable/reusable-banner";
-import { ProductInformationDesktopView } from "../../reusable/product-information-desktop-view";
-import { ProductInformationMobileView } from "../../reusable/product-information-mobile-view";
-import { ProductImagesDesktopView } from "../../reusable/product-images-desktop-view";
-import { Footer } from "../../reusable/footer";
+import { ReusableBanner } from "../../reusable/banner";
+import { ProductInformationDesktopView } from "./product-information-desktop-view";
+import { ProductInformationMobileView } from "./product-information-mobile-view";
+import { ProductImagesDesktopView } from "./product-images-desktop-view";
+import {Footer} from "../../reusable/footer"
 import "./index.css";
 
 //Renders page with product selected
 export function ShowOneProduct(props) {
   return (
-    <div className="desktop-view">
-      <NavBar />
-      <ReusableBanner />
-      <div className="product-container-desktop-view">
-        <ProductImagesDesktopView />
-        <ProductInformationDesktopView />
+    <React.Fragment>
+      <div className="desktop-view">
+        <NavBar />
+        <ReusableBanner />
+        <div className="product-container-desktop-view">
+          <ProductImagesDesktopView />
+          <ProductInformationDesktopView />
+        </div>
+        <Footer />
       </div>
-      <div className="product-container-mobile-view">
-        <ProductInformationMobileView />
+//dispatch to get one product
+//import 
+
+      <div className="mobile-view">
+        <NavBar />
+        <ReusableBanner />
+        <div className="product-container-mobile-view">
+          <ProductInformationMobileView />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </React.Fragment>
   );
 }
+
