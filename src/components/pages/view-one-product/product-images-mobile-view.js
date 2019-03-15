637278c88b6e25ee83aeb456985cf373e549060f
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-14 19:07:51 -0700

    passed props to display information of product  + removed comment

--- src/components/pages/view-one-product/product-images-mobile-view.js
+++ src/components/pages/view-one-product/product-images-mobile-view.js
@@ -1,55 +1,31 @@
 import React from "react";
 
-import { connect } from "react-redux";
+
 import "./product-images-mobile-view.css";
 
 //Renders mobile viewport layout for product images
 export class ProductImagesMobileView extends React.Component {
+  constructor(props) {
+    super(props);
+    //   this.state = {
+    //     selectedImage: props.product.imageUrl
+    //   };
+  }
   render() {
-    const products = this.props.products.map(
-      product => {
-        return (
-          <React.Fragment>
-            <div className="images-container">
-              <div className="hero-image">
-                <img src={product.imageUrl} alt={product.shortDescription} />
-              </div>
-              <div className="bottom-thumbnails-container">
-                <a className="thumbnails" href="#">
-                  <img
-                    src={product.secondImage}
-                    alt={product.shortDescription}
-                  />
-                </a>
-                <a className="thumbnails" href="#">
-                  <img
-                    src={product.thirdImage}
-                    alt={product.shortDescription}
-                  />
-                </a>
-              </div>
-            </div>
-          </React.Fragment>
-        );
-      }
+    return (
+      <div className="images-container">
+        <div className="hero-image">
+          <img src={this.props.product.imageUrl} alt={this.props.product.shortDescription} />
+        </div>
+        <div className="bottom-thumbnails-container">
+          <a className="thumbnails" href="#">
+            <img src={this.props.product.secondImage} alt={this.props.product.shortDescription} />
+          </a>
+          <a className="thumbnails" href="#">
+            <img src={this.props.product.thirdImage} alt={this.props.product.shortDescription} />
+          </a>
+        </div>
+      </div>
     );
-    return <React.Fragment>{products}</React.Fragment>;
   }
 }
-
-const mapStateToProps = state => ({
-  products: state.products.products
-});
-
-export default connect(mapStateToProps)(ProductImagesMobileView);
-
-
-//TRANSLATE THIS TO REACT
-// function handleThumbnailClicks() {
-//     $('.thumbnail').on('click', function(event) {
-//        const imgSrc = $(this).find('img').attr('src'); 
-//        const imgAlt = $(this).find('img').attr('alt');
-//     $('.hero img').attr({'src': imgSrc, 'alt': imgAlt});
-//     });
-//   }
-//   $(handleThumbnailClicks);
