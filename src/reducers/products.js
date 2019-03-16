3d78de726f48ebaf09c0c014084ca411a3d826a4
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-16 14:12:14 -0700

    REDUX: Created reducer for delete dispatch

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -4,7 +4,9 @@ import {
   FETCH_ONE_PRODUCT_SUCCESS,
   FETCH_ONE_PRODUCT_ERROR,
   POST_ITEM_SUCCESS,
-  POST_ITEM_ERROR
+  POST_ITEM_ERROR,
+  DELETE_ITEM_SUCCESS,
+  DELETE_ITEM_ERROR
 } from "../actions/products";
 
 const initialState = {
@@ -43,6 +45,15 @@ export default function reducer(state = initialState, action) {
       error: null
     });
   } else if (action.type === POST_ITEM_ERROR) {
+    return Object.assign({}, state, {
+      error: action.error
+    });
+  } else if (action.type === DELETE_ITEM_SUCCESS) {
+    return Object.assign({}, state, {
+      item: action.item,
+      error: null
+    });
+  } else if (action.type === DELETE_ITEM_ERROR) {
     return Object.assign({}, state, {
       error: action.error
     });
