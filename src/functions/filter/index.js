4dfb8b1604cc9e50e66b6576ebe49c8e7297886f
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-17 09:32:34 -0700

    TOOLS: Function to filter by style

---
+++ src/functions/filter/index.js
@@ -0,0 +1,7 @@
+const OnePiecefilter = `${product.style}`.filter(
+  style => style === "one-piece bikini"
+);
+
+const TwoPiecefilter = `${product.style}`.filter(
+  style => style === "two-piece bikini"
+);
