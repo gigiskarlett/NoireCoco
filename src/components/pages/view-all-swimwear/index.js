b63c3ba17a73a2ac07e9d8b60fef88a172120bd4
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-18 10:26:25 -0700

    COMPONENT: Passed props to filter component

--- src/components/pages/view-all-swimwear/index.js
+++ src/components/pages/view-all-swimwear/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { connect } from "react-redux";
+import { getAllProducts } from "../../../actions/products";
 import { NavBar } from "../../reusable/navBar";
 import { EmailCapture } from "../../reusable/email-capture";
 import { Filter } from "../../reusable/filter";
@@ -10,6 +11,9 @@ import { Footer } from "../../reusable/footer";
 import "./index.css";
 
 export class ViewAllSwimwear extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(getAllProducts());
+  }
   render() {
     return (
       <React.Fragment>
@@ -17,7 +21,7 @@ export class ViewAllSwimwear extends React.Component {
         <div className="pink-banner">
           <EmailCapture />
         </div>
-        <Filter />
+        <Filter product={this.props.product}/>
         <section className="swimwear-products">
           <Products />
         </section>
@@ -26,5 +30,8 @@ export class ViewAllSwimwear extends React.Component {
     );
   }
 }
+const mapStateToProps = state => ({
+  product: state.products.products
+});
 
-export default connect()(ViewAllSwimwear);
+export default connect(mapStateToProps)(ViewAllSwimwear);
