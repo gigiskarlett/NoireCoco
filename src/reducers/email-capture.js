e2ba0805d2b9662a524ce2883fe225aa78d3b2ca
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-18 23:04:23 -0700

    REDUX: Reducer for success/error for email subscription

--- src/reducers/email-capture.js
+++ src/reducers/email-capture.js
@@ -7,3 +7,17 @@ const initialState = {
   email: "",
   error: null
 };
+
+export default function reducer(state = initialState, action) {
+  if (action.type === POST_SUBSCRIBE_ERROR) {
+    return Object.assign({}, state, {
+      error: action.error
+    });
+  } else if (action.type === POST_SUBSCRIBE_SUCCESS) {
+    return Object.assign({}, state, {
+      item: action.item,
+      error: null
+    });
+  }
+  return state;
+}
