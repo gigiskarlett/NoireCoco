b7306dc36ed2de3a7d65ff776001f77d82483c0b
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-19 13:30:07 -0700

    removed unusued import

--- src/components/pages/view-all-swimwear/products.js
+++ src/components/pages/view-all-swimwear/products.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {Link } from "react-router-dom";
 import "./product.css";
 
 //Renders Product with product images and short information
