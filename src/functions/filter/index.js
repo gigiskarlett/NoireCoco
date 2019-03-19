aca69a7a9eb2285bcbf8e3fd430a3618f7b784c3
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-19 16:10:15 -0700

    REMOVE: Removed unused file

--- src/functions/filter/index.js
+++
@@ -1,7 +0,0 @@
-export const onePiecefilter = `${product.style}`.filter(
-  style => style === "one-piece bikini"
-);
-
-export const twoPiecefilter = `${product.style}`.filter(
-  style => style === "two-piece bikini"
-);
