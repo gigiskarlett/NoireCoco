d66e7b86814ceaf588260a4cc9dd7ef97e2eb764
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-18 23:17:42 -0700

    COMPONENT: Created submission for form + added dispatch + clear fields after dispatch

--- src/components/reusable/email-capture.js
+++ src/components/reusable/email-capture.js
@@ -1,29 +1,59 @@
 import React from "react";
 
+import { postEmail } from "../../actions/email-capture";
+import { connect } from "react-redux";
 import "./email-capture.css";
 
 //Renders email capture to subscribe for updates
-export function EmailCapture() {
-  return (
-    <div className="email-container">
-      <div className="email-box-container">
-        <p className="call-to-action">Lets keep in touch</p>
-        <section>
-          <form className="email-capture" action="#">
-            <legend align="center" hidden>
-              Lets keep in touch? Subscribe
-            </legend>
-            <input
-              type="text"
-              placeholder="Enter your email address"
-              name="email-address-input"
-              className="email-address-input"
-              required
-            />
-            <input type="submit" className="join-button" value="Join" />
-          </form>
-        </section>
+class EmailCapture extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: ""
+    };
+  }
+
+  //handles submission of form and dispatches action to post api call
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.dispatch(postEmail(this.state)).then(() => this.clearFields());
+  }
+  clearFields() {
+    this.setState({
+      email: ""
+    });
+  }
+
+  render() {
+    return (
+      <div className="email-container">
+        <div className="email-box-container">
+          <p className="call-to-action">Lets keep in touch</p>
+          <section>
+            <form
+              className="email-capture"
+              aria-live="polite"
+              onSubmit={e => this.handleSubmit(e)}
+            >
+              <legend align="center" hidden>
+                Lets keep in touch? Subscribe
+              </legend>
+              <input
+                type="text"
+                placeholder="Enter your email address"
+                name="email-address-input"
+                className="email-address-input"
+                value={this.state.email}
+                onChange={e => this.setState({ email: e.target.value })}
+                required
+              />
+              <input type="submit" className="join-button" value="Join" />
+            </form>
+          </section>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default connect()(EmailCapture);
