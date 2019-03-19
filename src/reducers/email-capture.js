3faa0afef95ad8ffd2f853648d6059d8797225e7
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-18 23:14:18 -0700

    REDUX: Fixed import + Fixed typo

--- src/reducers/email-capture.js
+++ src/reducers/email-capture.js
@@ -1,7 +1,7 @@
 import {
   POST_SUBSCRIBE_SUCCESS,
   POST_SUBSCRIBE_ERROR
-} from "../actions/products";
+} from "../actions/email-capture";
 
 const initialState = {
   email: "",
@@ -15,7 +15,7 @@ export default function reducer(state = initialState, action) {
     });
   } else if (action.type === POST_SUBSCRIBE_SUCCESS) {
     return Object.assign({}, state, {
-      item: action.item,
+      email: action.email,
       error: null
     });
   }
