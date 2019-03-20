68af109bce1e9b2e5df5d17631c76ca676ff8d86
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-19 21:51:24 -0700

    COMPONENT:  Imported modules needed for dispatch + authentication + input component

---
+++ src/components/pages/login/login-form.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Field, reduxForm, focus } from "redux-form";
+import Input from "../../reusable/input";
+import { login } from "../../../actions/auth";
+import { required, nonEmpty } from "../../../validators";
+
+export class LoginForm extends React.Component {
+  render() {}
+}
+
