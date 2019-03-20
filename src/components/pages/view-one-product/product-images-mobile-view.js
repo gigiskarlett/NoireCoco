7191415c1cd878559968055e1117b79ef4bbb4bc
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-19 23:19:22 -0700

    COMPONENT: Commented out component

--- src/components/pages/view-one-product/product-images-mobile-view.js
+++ src/components/pages/view-one-product/product-images-mobile-view.js
@@ -5,12 +5,12 @@ import "./product-images-mobile-view.css";
 
 //Renders mobile viewport layout for product images
 export class ProductImagesMobileView extends React.Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+    // super(props);
     //   this.state = {
     //     selectedImage: props.product.imageUrl
     //   };
-  }
+  // }
   render() {
     return (
       <div className="images-container">
