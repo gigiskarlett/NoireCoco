3b687828d1bf93bd95e5e6660fdfd96a277677ee
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-19 17:43:11 -0700

    STYLE: Added classnames for styling

--- src/components/pages/registration/registration-form.js
+++ src/components/pages/registration/registration-form.js
@@ -21,36 +21,46 @@ class RegistrationForm extends React.Component {
   render() {
     return (
       <form
-        className="login-form"
+        className="registration-form"
         onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
       >
         <h2 className="register-form-title">REGISTER</h2>
-        <label htmlFor="firstName">First name</label>
+        <label className="registration-label" htmlFor="firstName">
+          First name
+        </label>
         <Field component={Input} type="text" name="firstName" />
-        <label htmlFor="lastName">Last name</label>
+        <label className="registration-label" htmlFor="lastName">
+          Last name
+        </label>
         <Field component={Input} type="text" name="lastName" />
-        <label htmlFor="username">Username</label>
+        <label className="registration-label" htmlFor="username">
+          Username
+        </label>
         <Field
           component={Input}
           type="text"
           name="username"
           validate={[required, nonEmpty, isTrimmed]}
         />
-        <label htmlFor="password">Password</label>
+        <label className="registration-label" htmlFor="password">
+          Password
+        </label>
         <Field
           component={Input}
           type="password"
           name="password"
           validate={[required, passwordLength, isTrimmed]}
         />
-        <label htmlFor="passwordConfirm">Confirm password</label>
+        <label className="registration-label" htmlFor="passwordConfirm">
+          Confirm password
+        </label>
         <Field
           component={Input}
           type="password"
           name="passwordConfirm"
           validate={[required, nonEmpty, matchesPassword]}
         />
-        <button
+        <button className="registration-button"
           type="submit"
           disabled={this.props.pristine || this.props.submitting}
         >
@@ -62,7 +72,7 @@ class RegistrationForm extends React.Component {
 }
 
 export default reduxForm({
-    form: 'registration',
-    onSubmitFail: (errors, dispatch) =>
-        dispatch(focus('registration', Object.keys(errors)[0]))
+  form: "registration",
+  onSubmitFail: (errors, dispatch) =>
+    dispatch(focus("registration", Object.keys(errors)[0]))
 })(RegistrationForm);
