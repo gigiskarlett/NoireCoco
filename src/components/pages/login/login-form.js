b0d66662f01ff26bfff5cf28f7667407380449a3
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-19 21:59:41 -0700

    STYLE: Imported modules for: Footer + NavBar

--- src/components/pages/login/login-form.js
+++ src/components/pages/login/login-form.js
@@ -1,4 +1,7 @@
 import React from "react";
+
+import { NavBar } from "../../reusable/navBar";
+import { Footer } from "../../reusable/footer";
 import { Field, reduxForm, focus } from "redux-form";
 import Input from "../../reusable/input";
 import { login } from "../../../actions/auth";
@@ -14,43 +17,46 @@ export class LoginForm extends React.Component {
         </div>
       );
     }
-    return (<React.Fragment>
-      <div className="login-form-container">
-        <form
-          className="login-form"
-          onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
-        >
-          {error}
-          <label className="login-form-label" htmlFor="username">
-            Username
-          </label>
-          <Field
-            component={Input}
-            type="text"
-            name="username"
-            id="username"
-            validate={[required, nonEmpty]}
-          />
-          <label className="login-form-label" htmlFor="password">
-            Password
-          </label>
-          <Field
-            component={Input}
-            type="password"
-            name="password"
-            id="password"
-            validate={[required, nonEmpty]}
-          />
-          <div className="login-button-container">
-            <button
-              className="login-button"
-              disabled={this.props.pristine || this.props.submitting}
-            >
-              Log in
-            </button>
-          </div>
-        </form>
-      </div>
+    return (
+      <React.Fragment>
+        <NavBar />
+        <div className="login-form-container">
+          <form
+            className="login-form"
+            onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
+          >
+            {error}
+            <label className="login-form-label" htmlFor="username">
+              Username
+            </label>
+            <Field
+              component={Input}
+              type="text"
+              name="username"
+              id="username"
+              validate={[required, nonEmpty]}
+            />
+            <label className="login-form-label" htmlFor="password">
+              Password
+            </label>
+            <Field
+              component={Input}
+              type="password"
+              name="password"
+              id="password"
+              validate={[required, nonEmpty]}
+            />
+            <div className="login-button-container">
+              <button
+                className="login-button"
+                disabled={this.props.pristine || this.props.submitting}
+              >
+                Log in
+              </button>
+            </div>
+          </form>
+        </div>
+        <Footer />
       </React.Fragment>
     );
   }
