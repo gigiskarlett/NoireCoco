fc0a1f78deed5ac86c057c6b196b5f259b332052
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-19 17:22:06 -0700

    COMPONENT: Updated path for import

--- src/components/pages/registration/registration-form.js
+++ src/components/pages/registration/registration-form.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Field, reduxForm, focus } from "redux-form";
 import { registerUser } from "../../../actions/users";
 import { login } from "../../../actions/auth";
-import Input from "./input";
+import Input from "../../reusable/input";
 import "./registration-form.css";
 import { required, nonEmpty, matches, length, isTrimmed } from "./validators";
 const passwordLength = length({ min: 6, max: 72 });
