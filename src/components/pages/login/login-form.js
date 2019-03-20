ae8f9b8d23a36e463341ad82557abd76bd50f2d4
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-19 21:58:32 -0700

    COMPONENT: Created form for login

--- src/components/pages/login/login-form.js
+++ src/components/pages/login/login-form.js
@@ -5,6 +5,58 @@ import { login } from "../../../actions/auth";
 import { required, nonEmpty } from "../../../validators";
 
 export class LoginForm extends React.Component {
-  render() {}
+  render() {
+    let error;
+    if (this.props.error) {
+      error = (
+        <div className="form-error" aria-live="polite">
+          {this.props.error}
+        </div>
+      );
+    }
+    return (<React.Fragment>
+      <div className="login-form-container">
+        <form
+          className="login-form"
+          onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
+        >
+          {error}
+          <label className="login-form-label" htmlFor="username">
+            Username
+          </label>
+          <Field
+            component={Input}
+            type="text"
+            name="username"
+            id="username"
+            validate={[required, nonEmpty]}
+          />
+          <label className="login-form-label" htmlFor="password">
+            Password
+          </label>
+          <Field
+            component={Input}
+            type="password"
+            name="password"
+            id="password"
+            validate={[required, nonEmpty]}
+          />
+          <div className="login-button-container">
+            <button
+              className="login-button"
+              disabled={this.props.pristine || this.props.submitting}
+            >
+              Log in
+            </button>
+          </div>
+        </form>
+      </div>
+      </React.Fragment>
+    );
+  }
 }
 
+export default reduxForm({
+  form: "login",
+  onSubmitFail: (errors, dispatch) => dispatch(focus("login", "username"))
+})(LoginForm);
