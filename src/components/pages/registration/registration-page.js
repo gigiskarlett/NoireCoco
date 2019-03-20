9efbff8b69c6d0591941dc49da8bc4f2e8b2da37
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-19 17:18:35 -0700

    COMPONENT: imported registartion form, navbar, footer.

---
+++ src/components/pages/registration/registration-page.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {Link, Redirect} from 'react-router-dom';
+
+import RegistrationForm from './registration-form';
+import { NavBar } from '../../reusable/navBar';
+import { Footer } from '../../reusable/footer';
+
+export function RegistrationPage(props) {
+    return (
+        <div className="admin-home">
+            <NavBar/>
+            <h2>ADMINISTRATIVE PAGE FOR NOIRE COCO</h2>
+            <RegistrationForm />
+            <Link to="/admin/login">Login</Link>
+            <Footer/>
+        </div>
+    );
+}
+
