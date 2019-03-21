97a2f9f24445b11154b8a397eded48539fbaa6ba
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-21 14:49:24 -0700

    REDUX: Added dispatch to logout user

--- src/components/reusable/admin-page-nav-bar.js
+++ src/components/reusable/admin-page-nav-bar.js
@@ -1,26 +1,42 @@
 import React from "react";
 
+import { connect } from "react-redux";
+import { clearAuth } from "../../actions/auth";
 import { Link } from "react-router-dom";
 import "./admin-page-nav-bar.css";
 
 //Renders navigation bar for admin pages
-export function AdminPagesNavBar() {
-  return (
-    <nav>
-      <Link to="/">
-        <img
-          className="logo"
-          src="https://i.ibb.co/VNZVnvr/imageedit-35-5868682626.png"
-          alt="Noire Coco logo"
-        />
-      </Link>
-      <ul className="admin-nav-links">
-        <li className="logout-link">
-          <Link className="link" aria-label="Logout" to="/">
-            Logout
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
+class AdminPagesNavBar extends React.Component {
+  logout() {
+    this.props.dispatch(clearAuth());
+  }
+  render() {
+    return (
+      <nav>
+        <Link to="/">
+          <img
+            className="logo"
+            src="https://i.ibb.co/VNZVnvr/imageedit-35-5868682626.png"
+            alt="Noire Coco logo"
+          />
+        </Link>
+        <ul className="admin-nav-links">
+          <li className="logout-link">
+            <p
+              className="link"
+              aria-label="Logout"
+              onClick={e => {
+                e.preventDefault();
+                this.logout();
+              }}
+            >
+              Logout
+            </p>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 }
+
+export default connect()(AdminPagesNavBar);
