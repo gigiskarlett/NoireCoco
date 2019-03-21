0831de045ccb856d2368a6f6e933f5ee4a9b1ad9
Gianina Skarlett <skarlett.gianina@gmail.com>
2019-03-21 15:20:49 -0700

    COMPONENT: Removed usused imports

--- src/components/pages/login/login-form.js
+++ src/components/pages/login/login-form.js
@@ -1,11 +1,8 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
-import { NavBar } from "../../reusable/navBar";
-import { Footer } from "../../reusable/footer";
 import { Field, reduxForm, focus } from "redux-form";
 import Input from "../../reusable/input";
-import { Redirect } from "react-router-dom";
 import { login } from "../../../actions/auth";
 import { required, nonEmpty } from "../../../validators";
 import "./login-form.css";
